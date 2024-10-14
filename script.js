56d0b667b8e5e24524b90d8154ac5a3779f7f72d
“tabihsfaisal“ <tabish.faisaltn@gmail.com>
2024-10-14 17:51:14 +0200

    fatto es-biglietto

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 //Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
 //Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-let eta = parseInt(prompt("Inserisci la tua età:"));
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
-const prezzoBase = '0,21';
-
 // va applicato uno sconto del 20% per i minorenni
-sconto = 0.21 * 20 / 100
-console.log(sconto)
 // va applicato uno sconto del 40% per gli over 65.
+
+
+// chiedere l'età dell'utente
+let eta = parseInt(prompt("Inserisci la tua età:"));
+
+// chiedere km all'utente
+let percorso = parseInt(prompt("Inserisci kilometri che vuole percorrere:"));
+
+// dare il prezzobase per i kilometri al cliente
+const prezzoBase = 0.21; // prezzobase è 0.21 al kilometro
+
+// calcolo il prezzo per il cliente
+const prezzoFinaleNodiscount = 0.21 * percorso // il prezzoFinale Sarà il prodotto di prezzoBase e kilometri da percorrere
+
+let sconto= 0
+
+if (eta < 18){ // 20% di sconto
+    sconto = prezzoFinaleNodiscount * (20/100);
+    console.log("sei minorenne")
+}
+else if (eta >= 65){
+    sconto = prezzoFinaleNodiscount * (40/100);
+    console.log("sei over 65")
+}
+
+let prezzoFinale= prezzoFinaleNodiscount - sconto
+console.log(Math.round(prezzoFinale)); // uso method Math.round per avere prezzofinale in numeri normale non decimali
+
+
